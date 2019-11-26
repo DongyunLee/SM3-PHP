8a27cdf2b52dbb04c373c89306011ef75cda8bca
ch4o5 <1150477170@qq.com>
2019-11-26 17:23:53 +0800

    * 更新了changelog * 新增了语法糖binOr/binNot/binXor/binAnd/binShiftLeft

--- src/functions.php
+++ src/functions.php
@@ -30,8 +30,74 @@ function sm3($message)
  *
  * @return string
  */
-function bin_add($params)
+function binAdd($params)
 {
     $bin_sum = WordConversion::addConversion($params);
     return (string)$bin_sum;
+}
+
+/**
+ * 二进制按位与运算
+ *
+ * @param $params
+ *
+ * @return string
+ */
+function binAnd($params)
+{
+    $bin_and = WordConversion::andConversion($params);
+    return (string)$bin_and;
+}
+
+/**
+ * 二进制按位或运算
+ *
+ * @param $params
+ *
+ * @return string
+ */
+function binOr($params)
+{
+    $bin_or = WordConversion::orConversion($params);
+    return (string)$bin_or;
+}
+
+/**
+ * 二进制按位异或运算
+ *
+ * @param $params
+ *
+ * @return string
+ */
+function binXor($params)
+{
+    $bin_xor = WordConversion::xorConversion($params);
+    return (string)$bin_xor;
+}
+
+/**
+ * 二进制按位非运算
+ *
+ * @param $binary
+ *
+ * @return string
+ */
+function binNot($binary)
+{
+    $bin_not = WordConversion::notConversion($binary);
+    return (string)$bin_not;
+}
+
+/**
+ * 二进制按位左移指定的位数
+ *
+ * @param $binary
+ * @param $times
+ *
+ * @return string
+ */
+function binShiftLeft($binary, $times)
+{
+    $bin_left_pad = WordConversion::shiftLeftConversion($binary, $times);
+    return (string)$bin_left_pad;
 }
