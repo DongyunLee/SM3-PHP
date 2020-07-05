35d58e9b23a19dcc733dddcd29f8ba70ac2f6347
ch4o5 <dongyunli619@gmail.com>
2020-07-05 17:19:54 +0800

    * 给composer.json添加了阿里云镜像 * 添加了测试用例

--- tests/SM3Test.php
+++ tests/SM3Test.php
@@ -32,7 +32,8 @@ class SM3Test extends PHPUnit_Framework_TestCase
     {
         return array(
             array('abc', '66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0'),
-            array('abcdef', '5d60e23c9fe29b5e62517e144ad67541c6eb132c8926637b6393fe8d9b62b3bf')
+            array('abcdef', '5d60e23c9fe29b5e62517e144ad67541c6eb132c8926637b6393fe8d9b62b3bf'),
+            array('a a','d6ef141c5faa9bbde67cbc9f45988d6158eaf0bc2ab492bb489a6524ca492cbc')
         );
     }
 }
